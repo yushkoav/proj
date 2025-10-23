7f497bce2b97eb1187a64932156945048e402029
Alex Kripto <yushkoav@gmail.com>
2025-10-23 18:13:04 +0300

    Выполнены улучшения Добавлено две строки

--- src/main/java/mainapp.java
+++ src/main/java/mainapp.java
@@ -1,5 +1,7 @@
 public class mainapp {
     public static void main(String[] args) {
         System.out.println("Hallow world");
+        System.out.println("Hallow world");
+        System.out.println("Hallow world");
     }
 }
